f05d97543de36a6e3e6e7a74547c54d259e7b082
Josh Triplett <josh@freedesktop.org>
2006-04-27 14:14:54 -0700

    Add test program for XFree86-DRI extension to xcb-demo.  Mark XFree86-DRI extension as tested and working.

---
+++ xcbxf86dri.c
@@ -0,0 +1,50 @@
+/* Copyright (C) 2006 Josh Triplett. All Rights Reserved.  See the file
+ * COPYING in this directory for licensing information. */
+#include <X11/XCB/xcb.h>
+#include <X11/XCB/xf86dri.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int screen;
+    XCBXF86DriQueryVersionCookie qvc;
+    XCBXF86DriQueryVersionRep *qv;
+    XCBXF86DriQueryDirectRenderingCapableCookie qdrc;
+    XCBXF86DriQueryDirectRenderingCapableRep *qdr;
+    XCBConnection *c = XCBConnect(NULL, &screen);
+
+    if(!c)
+    {
+        fprintf(stderr, "Error establishing connection to X server.");
+        return 1;
+    }
+
+    qvc  = XCBXF86DriQueryVersion(c);
+    qdrc = XCBXF86DriQueryDirectRenderingCapable(c, screen);
+
+    qv  = XCBXF86DriQueryVersionReply(c, qvc, 0);
+    if(!qv)
+    {
+        fprintf(stderr, "Error querying DRI extension version.\n");
+        return 1;
+    }
+    printf("DRI extension version: %hu.%hu.%u\n",
+           qv->dri_major_version, qv->dri_minor_version, qv->dri_minor_patch);
+    free(qv);
+
+    qdr = XCBXF86DriQueryDirectRenderingCapableReply(c, qdrc, 0);
+    if(!qdr)
+    {
+        fprintf(stderr, "Error querying direct rendering capability.\n");
+        return 1;
+    }
+    printf("Direct rendering (screen %d): %s\n",
+           screen, qdr->is_capable ? "yes" : "no");
+    free(qdr);
+
+    XCBDisconnect(c);
+
+    return 0;
+}
